91522c26100931a3d5ae7622df61ba1f8238be7f
Avijeet pandey <avijeetpandey25@gmail.com>
2024-03-20 21:55:56 +0530

    Add: Profile header view

---
+++ Shorts/Core/Profile/CurrentUserProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentUserProfileView.swift
+//  Shorts
+//
+//  Created by Avijeet on 20/03/24.
+//
+
+import SwiftUI
+
+struct CurrentUserProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrentUserProfileView()
+}
---
+++ Shorts/Core/Profile/ProfileGridView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileGridView.swift
+//  Shorts
+//
+//  Created by Avijeet on 20/03/24.
+//
+
+import SwiftUI
+
+struct ProfileGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileGridView()
+}
