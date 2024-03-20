2763229bd7e94799be5f58ec850054a21273bdb6
Avijeet pandey <avijeetpandey25@gmail.com>
2024-03-20 21:16:36 +0530

    Add: Side view buttons

--- Shorts/Core/Feed/FeedCell.swift
+++ Shorts/Core/Feed/FeedCell.swift
@@ -18,6 +18,97 @@ struct FeedCell: View {
                     Text("Post \(post)")
                         .foregroundStyle(.white)
                 }
+            
+            VStack {
+                Spacer()
+                
+                HStack(alignment: .bottom) {
+                    VStack(alignment: .leading) {
+                        Text("Avijeet Pandey")
+                            .fontWeight(.semibold)
+                        
+                        Text("Rocket ship preparing to take off")
+                            .fontWeight(.semibold)
+                    }.font(.subheadline)
+                        .foregroundStyle(.white)
+                    
+                    Spacer()
+                    
+                    VStack(spacing: 28) {
+                        
+                        Circle()
+                            .frame(width: 48, height: 48)
+                            .foregroundStyle(.gray)
+                        
+                        Button {
+                            
+                        } label: {
+                            VStack {
+                                Image(systemName: "heart.fill")
+                                    .resizable()
+                                    .frame(width: 28,height: 28)
+                                    .foregroundStyle(.white)
+                                
+                                Text("12")
+                                    .font(.footnote)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            VStack {
+                                Image(systemName: "ellipsis.bubble.fill")
+                                    .resizable()
+                                    .frame(width: 28,height: 28)
+                                    .foregroundStyle(.white)
+                                
+                                Text("27")
+                                    .font(.footnote)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            VStack {
+                                Image(systemName: "bookmark.fill")
+                                    .resizable()
+                                    .frame(width: 22,height: 28)
+                                    .foregroundStyle(.white)
+                                
+                                Text("2")
+                                    .font(.footnote)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            VStack {
+                                Image(systemName: "arrowshape.turn.up.right.fill")
+                                    .resizable()
+                                    .frame(width: 28,height: 28)
+                                    .foregroundStyle(.white)
+                                
+                                
+                                Text("30")
+                                    .font(.footnote)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                            
+                        }
+                    }
+                }
+                
+            }.padding()
         }
     }
 }
