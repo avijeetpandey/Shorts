4288a0b35618e5edad82440c39e22415448ff623
Avijeet pandey <avijeetpandey25@gmail.com>
2024-03-20 19:31:42 +0530

    Add: Tab bar selected states

--- Shorts/Core/TabBar/MainTabView.swift
+++ Shorts/Core/TabBar/MainTabView.swift
@@ -8,21 +8,28 @@
 import SwiftUI
 
 struct MainTabView: View {
+    
+    @State private var selectedTab = 0
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             Text("Home").tabItem {
                 VStack {
-                    Image(systemName: "house")
+                    Image(systemName: selectedTab == 0 ? "house.fill" : "house")
+                        .environment(\.symbolVariants, selectedTab == 0 ? .fill : .none)
                     Text("Home")
                 }
-            }
+            }.onAppear { selectedTab = 0}
+                .tag(0)
             
             Text("Explorer").tabItem {
                 VStack {
-                    Image(systemName: "person.2")
+                    Image(systemName: selectedTab == 1 ? "person.2.fill" : "person.2")
+                        .environment(\.symbolVariants, selectedTab == 1 ? .fill : .none)
                     Text("Friends")
                 }
-            }
+            }.onAppear { selectedTab = 1}
+                .tag(1)
             
             Text("Upload Post").tabItem {
                 VStack {
@@ -32,17 +39,21 @@ struct MainTabView: View {
             
             Text("Notifications").tabItem {
                 VStack {
-                    Image(systemName: "bell")
-                    Text("Notifications")
+                    Image(systemName: selectedTab == 3 ? "bell.fill" : "bell")
+                        .environment(\.symbolVariants, selectedTab == 3 ? .fill : .none)
+                    Text("Inbox")
                 }
-            }
+            }.onAppear { selectedTab = 3}
+                .tag(3)
             
             Text("Profile").tabItem {
                 VStack {
-                    Image(systemName: "person")
+                    Image(systemName: selectedTab == 4 ? "person.fill" : "person")
+                        .environment(\.symbolVariants, selectedTab == 4 ? .fill : .none)
                     Text("Profile")
                 }
-            }
+            }.onAppear { selectedTab = 4}
+                .tag(4)
         }
     }
 }
