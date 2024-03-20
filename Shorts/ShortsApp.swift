56584d83187102a734d63b8bff6d5dcf3ae07f8a
Avijeet pandey <avijeetpandey25@gmail.com>
2024-03-20 19:06:24 +0530

    Add: Init projects

---
+++ Shorts/ShortsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortsApp.swift
+//  Shorts
+//
+//  Created by Avijeet on 20/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ShortsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
